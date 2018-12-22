de8a8f06275a4fcedcfafb91ffd8ed214780f577
M Lukman Hakim <lukman821@gmail.com>
2018-12-22 23:07:11 +0800

    String Interpolation

---
+++ bin/StringInterpolation.dart
@@ -0,0 +1,21 @@
+
+
+void main(){
+  //Literals
+  var name = "Lukman";
+  var age = 24;
+
+  //various way to define String literal in dart
+
+  var desc = 'hello my name is lukman. im from canada,'
+      'i like to write programming language to build any application';
+
+  //String interpolation
+  String nama = "Lukman";
+
+  print("my name is $nama");
+  print("jumlah karakter dari $nama adalah ${nama.length}");
+
+
+
+}
