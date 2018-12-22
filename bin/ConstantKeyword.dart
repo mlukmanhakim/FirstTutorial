262069adcfde4fbf399fd1963f12cce348d8fa42
M Lukman Hakim <lukman821@gmail.com>
2018-12-22 23:19:50 +0800

    Constants using final and const keyword

---
+++ bin/ConstantKeyword.dart
@@ -0,0 +1,14 @@
+
+void main(){
+
+//  final keyword
+    final cityName = "jakarta";
+//    cityName ="lombok";
+    const PI = 3.14;
+
+}
+
+class circle{
+  final color = 'red'; // final bisa di define pada kelas
+  static const PI = 3.14; // const tidak bisa didefine pada kelas
+}
