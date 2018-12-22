1a2fb6e72193337909066a31a6fca706b966dda8
M Lukman Hakim <lukman821@gmail.com>
2018-12-22 22:42:47 +0800

    Rr

--- bin/main.dart
+++ bin/main.dart
@@ -1,5 +1,23 @@
 
 
 void main() {
-  print('Hello world');
+  //Number : Int
+  int score = 20;
+  var count = 20; //it is inferred as integer automatically by compiler
+  // Number : Double
+  double precentage = 93.4;
+  //String
+
+  String name = "Lukman";
+
+  //Boolean
+  bool isValid = true;
+
+//  print
+  print(score);
+  print(precentage);
+  print(name);
+  print(isValid);
+  print(count);
+
 }
